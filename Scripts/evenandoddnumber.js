c1dace14da1eaa62d580933a94072b9c4b7f69fa
selfshuvo <shuvoahmed8121@gmail.com>
2024-04-04 23:56:25 +0600

    Update

--- Scripts/evenandoddnumber.js
+++ Scripts/evenandoddnumber.js
@@ -5,7 +5,7 @@ const output = document.getElementById("output");
 function eventChecker() {
   const number = input.value;
   if(number % 2 == 0) {
-   return output.innerText = 'Even Number';
+   return output.innerText = 'Even Number'
   }
   return output.innerText = 'Odd Number';
 
